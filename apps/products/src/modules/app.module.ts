63386029b4cfe28eec82956330d55c9935c55748
yuval4 <didi19289@gmail.com>
2025-04-01 22:29:24 +0300

    feat(app-module): integrate GraphQL with Apollo driver

--- apps/products/src/modules/app.module.ts
+++ apps/products/src/modules/app.module.ts
@@ -1,8 +1,23 @@
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 
+// @Module({
+//   imports: [],
+//   providers: [ProductsModule],
+// })
+// export class AppModule {}
+
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // autoSchemaFile: true, // Automatically generates the schema
+    }),
+  ],
   providers: [ProductsModule],
 })
 export class AppModule {}
