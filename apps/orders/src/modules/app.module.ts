a92ef2310775850ced23aef35f687c1e3cb99d28
yuval4 <didi19289@gmail.com>
2025-04-05 17:07:04 +0300

    add mongo

--- apps/orders/src/modules/app.module.ts
+++ apps/orders/src/modules/app.module.ts
@@ -5,10 +5,12 @@ import {
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { OrdersModule } from './orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     OrdersModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
     GraphQLModule.forRoot<ApolloFederationDriverConfig>({
       driver: ApolloFederationDriver,
       autoSchemaFile: { federation: 2 },
