773fbcb7f2624020638cfb0bed1ee0392e97b0d2
yuval4 <yuvalbar03@gmail.com>
2025-04-21 17:46:58 +0300

    remove all files

--- apps/orders/src/modules/orders/dto/update-order.input.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType, PartialType } from '@nestjs/graphql';
-import { CreateOrderInput } from './create-order.input';
-
-@InputType()
-export class UpdateOrderInput extends PartialType(CreateOrderInput) {
-  @Field(() => String)
-  _id: string;
-}
--- apps/products/src/modules/products/dto/update-product.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType, PartialType } from '@nestjs/graphql';
-import { Product } from '../entities/product.entity';
-import { CreateProductInput } from './create-product.input';
-
-@InputType()
-export class UpdateProductInput extends PartialType(CreateProductInput) {
-  @Field(() => String)
-  _id: Product['_id'];
-}
